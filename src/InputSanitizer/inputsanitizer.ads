5a1414758471a62e3b66302b7aeaaf7ac3c2e06a
Filip Gajowniczek <gajowniczek@adacore.com>
2022-02-04 10:44:53 -0600

    Added spec for InputSanitizer package

--- src/InputSanitizer/inputsanitizer.ads
+++ src/InputSanitizer/inputsanitizer.ads
@@ -1,5 +1,10 @@
+with Ada.Real_Time; 
+
 package InputSanitizer is
 
+   type Input_Range is range 0 .. 10;
    
+   -- Read which blocks until specified Time_Span elapses
+   function Read (Timeout : Ada.Real_Time.Time_Span; Read_Value : out Input_Range) return Boolean;
 
 end InputSanitizer;
