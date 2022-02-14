448b3a950658efe1045d9fbdfc433c4ea6d8d6ed
Filip Gajowniczek <gajowniczek@adacore.com>
2022-02-13 21:01:21 -0600

    Output of stub generation for Input_Processor package specification

---
+++ src/input_processor.adb
@@ -0,0 +1,27 @@
+pragma Ada_2012;
+package body Input_Processor is
+
+   -------------
+   -- Process --
+   -------------
+
+   procedure Process (Input : InputSanitizer.Input_Range) is
+   begin
+      pragma Compile_Time_Warning (Standard.True, "Process unimplemented");
+      raise Program_Error with "Unimplemented procedure Process";
+   end Process;
+
+   ---------------------------
+   -- Get_Processing_Result --
+   ---------------------------
+
+   function Get_Processing_Result return String is
+   begin
+      pragma Compile_Time_Warning
+        (Standard.True, "Get_Processing_Result unimplemented");
+      return
+        raise Program_Error
+          with "Unimplemented function Get_Processing_Result";
+   end Get_Processing_Result;
+
+end Input_Processor;
