30e6eb15d24129a32536af9a43df0c82b7637e73
Filip Gajowniczek <gajowniczek@adacore.com>
2022-02-13 19:28:23 -0600

    Result of running GNATpp with default GNAT style and upper-case keyword GNATpp option"

--- src/InputSanitizer/inputsanitizer.adb
+++ src/InputSanitizer/inputsanitizer.adb
@@ -1,20 +1,24 @@
 pragma Ada_2012;
 
-with Message_Buffers;            use Message_Buffers;
-with Serial_IO.Nonblocking;      use Serial_IO.Nonblocking;
-with Peripherals_Nonblocking;    use Peripherals_Nonblocking;
-with Ada.Real_Time;              use Ada.Real_Time;
+with Message_Buffers;         use Message_Buffers;
+with Serial_IO.Nonblocking;   use Serial_IO.Nonblocking;
+with Peripherals_Nonblocking; use Peripherals_Nonblocking;
+with Ada.Real_Time;           use Ada.Real_Time;
 
 package body InputSanitizer is
 
    Incoming : aliased Message (Physical_Size => 2);  -- arbitrary size
 
-   function Sanitize( Raw_Message : Message; Read_Value : out Input_Range ) return Boolean is
+   function Sanitize
+     (Raw_Message : Message; Read_Value : out Input_Range) return Boolean
+   is
    begin
-      if Raw_Message.Content_At(1) = '1' and then Raw_Message.Content_At(2) = '0' then
+      if Raw_Message.Content_At (1) = '1'
+        and then Raw_Message.Content_At (2) = '0'
+      then
          Read_Value := 10;
-      elsif Raw_Message.Content_At(2) in '0' .. '9' then
-         Read_Value := Input_Range'Value(Raw_Message.Content);
+      elsif Raw_Message.Content_At (2) in '0' .. '9' then
+         Read_Value := Input_Range'Value (Raw_Message.Content);
       else
          return False;
       end if;
@@ -38,7 +42,7 @@ package body InputSanitizer is
       if COM.Receiving then
          -- Serial port is still waiting to receive data after timeout
          return False;
-      elsif Sanitize(Incoming, Read_Value) then
+      elsif Sanitize (Incoming, Read_Value) then
          return True;
       else
          return False;
