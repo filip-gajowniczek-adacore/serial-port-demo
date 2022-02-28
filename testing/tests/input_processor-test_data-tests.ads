dc3e7f91c42f30a1a10b9e53a91b94304ec27b3f
Filip Gajowniczek <gajowniczek@adacore.com>
2022-02-28 14:58:06 -0600

    Output of gnattest --target=arm-eabi --RTS=light-tasking-stm32f4 -dd -P/home/gajowniczek/Documents/repositories/adacore-demos/serial-port-demo/default.gpr

---
+++ testing/tests/input_processor-test_data-tests.ads
@@ -0,0 +1,19 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Input_Processor.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Input_Processor.Test_Data.Test
+   with null record;
+
+   procedure Test_Process_c92564 (Gnattest_T : in out Test);
+   --  input_processor.ads:5:4:Process
+
+   procedure Test_Get_Processing_Result_23e640 (Gnattest_T : in out Test);
+   --  input_processor.ads:7:4:Get_Processing_Result
+
+end Input_Processor.Test_Data.Tests;
+--  end read only
